90b5aa7c3a5dcf1298a12bb252d976f18dd4f0d1
JoazinL2R2 <bilinskjoao@gmail.com>
2023-12-07 17:37:42 -0300

    ADD: Pagina para ver os cadastrado e pagina de login para o helpdesk

---
+++ Models/CadastroEquip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HD_SUPPORT.Models
+{
+    public class CadastroEquip
+    {
+        public int IdPatrimonio { get; set; }
+        public string Modelo { get; set; }
+        public string Processador { get; set; }
+        public string SistemaOperacionar { get; set; }
+        public string HeadSet { get; set; }
+        public int DtEmeprestimoInicio { get; set; }
+        public int DtEmeprestimoFinal { get; set; }
+    }
+}
