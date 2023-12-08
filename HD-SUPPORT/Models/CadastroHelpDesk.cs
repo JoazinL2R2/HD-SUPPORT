905495a6fe14cd11744da34492edb9b1b6b3bd9c
caiovogl <caiovogl9@gmail.com>
2023-12-08 17:03:16 -0300

    ADD: atualizando para .net 8 e criando banco de dados

---
+++ HD-SUPPORT/Models/CadastroHelpDesk.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace HD_SUPPORT.Models
+{
+    public class CadastroHelpDesk
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public byte[] Foto { get; set; }
+    }
+}
