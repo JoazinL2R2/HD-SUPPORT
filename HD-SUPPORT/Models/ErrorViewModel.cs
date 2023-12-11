1b7fdf5ac90336a7d1e483de81ccc6eee083c3ee
joao eduardo <bilinskjoao@gmail.com>
2023-12-11 15:20:33 -0300

    DELETE: Pasta bugada(eu criei errado)

--- HD-SUPPORT/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HD_SUPPORT.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
