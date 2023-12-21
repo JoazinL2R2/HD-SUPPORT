c0aaa74192d4d8685e41fb9ea2c46fcd2260e0d6
caiovogl <caiovogl9@gmail.com>
2023-12-21 15:44:30 -0300

    CHANGE: botão de excluir funcionário funcionando

--- HD-SUPPORT/obj/Debug/net8.0/HD-SUPPORT.AssemblyInfo.cs
+++ HD-SUPPORT/obj/Debug/net8.0/HD-SUPPORT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5d54845f3157e9d5639fa53a4bb5db60134efe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87b360651bea62387cee86d6c8213b6b1c82db36")]
 [assembly: System.Reflection.AssemblyProductAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
