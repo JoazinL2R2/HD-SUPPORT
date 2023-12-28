f82b378b820c0f65b30094b91d76f3178dab954b
João Bilinski Costa <145154461+JoazinL2R2@users.noreply.github.com>
2023-12-28 15:26:53 -0800

    Update: Layout

--- HD-SUPPORT/obj/Debug/net8.0/HD-SUPPORT.AssemblyInfo.cs
+++ HD-SUPPORT/obj/Debug/net8.0/HD-SUPPORT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24017fab2e79cfd5e883a59012632865086a918a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HD-SUPPORT/wwwroot/js/site.js
+++ HD-SUPPORT/wwwroot/js/site.js
@@ -1,1 +1,48 @@
-﻿
+﻿// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sideMenu = document.getElementById('sideMenu');
+    const iconMenu = document.querySelector('.icon_menu');
+    const closeMenuButton = document.querySelector('.close_menu');
+
+    // Adiciona o evento de clique ao ícone do menu
+    iconMenu.addEventListener('click', function (event) {
+        event.stopPropagation(); // Impede que o clique se propague para o documento
+        toggleMenu();
+    });
+
+    // Adiciona o evento de clique ao botão de fechar menu
+    closeMenuButton.addEventListener('click', function () {
+        toggleMenu();
+    });
+
+    // Fecha o menu se clicar fora dele
+    document.addEventListener('click', function (event) {
+        const isClickInsideMenu = sideMenu.contains(event.target);
+        const isClickOnIcon = event.target === iconMenu;
+
+        if (!isClickInsideMenu && !isClickOnIcon) {
+            closeMenu();
+        }
+    });
+
+    // Função para abrir o menu
+    function openMenu() {
+        sideMenu.style.right = '0px';
+    }
+
+    // Função para fechar o menu
+    function closeMenu() {
+        sideMenu.style.right = '-300px';
+    }
+
+    // Função para alternar entre abrir/fechar o menu
+    function toggleMenu() {
+        const isMenuOpen = sideMenu.style.right === '0px';
+        if (isMenuOpen) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    }
+});
