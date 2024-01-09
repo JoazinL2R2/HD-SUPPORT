2ca4933c18d7e5a9323508fe243e27080ad9871d
caiovogl <caiovogl9@gmail.com>
2024-01-09 17:35:26 -0300

    ADD: Filtragem de equipamentos por disponibilidade

--- HD-SUPPORT/obj/Debug/net8.0/HD-SUPPORT.AssemblyInfo.cs
+++ HD-SUPPORT/obj/Debug/net8.0/HD-SUPPORT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6faab3dc67dc5d0eb04b479797d15328b0575a31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c9e7b1638eed532f2947ea97793ad4667a4167")]
 [assembly: System.Reflection.AssemblyProductAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
