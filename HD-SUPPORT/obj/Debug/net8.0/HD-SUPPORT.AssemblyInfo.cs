45fa47e9797f3b9f78e809b4e05f8c0f6ba7bdc2
gugs(Gustavo.L) <123033640+gugsinwork@users.noreply.github.com>
2024-02-09 15:31:13 -0300

    UPDATE: geral pages

--- HD-SUPPORT/obj/Debug/net8.0/HD-SUPPORT.AssemblyInfo.cs
+++ HD-SUPPORT/obj/Debug/net8.0/HD-SUPPORT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5dfc0ae8808c9b970e725c5f6fb672ebd572365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9a91c67a94fed6c0a6b2b1cc2d6f8211c9c1b00")]
 [assembly: System.Reflection.AssemblyProductAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
