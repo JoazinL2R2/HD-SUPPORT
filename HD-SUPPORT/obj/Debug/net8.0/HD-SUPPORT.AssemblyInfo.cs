63343e03adffab40296a07add4790f4c119111e9
Joazin <Joazin@DESKTOP-0HK2QBU>
2024-02-15 23:06:38 -0300

    ADD: Modal de editar perfil

--- HD-SUPPORT/obj/Debug/net8.0/HD-SUPPORT.AssemblyInfo.cs
+++ HD-SUPPORT/obj/Debug/net8.0/HD-SUPPORT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9746f2ec1eccfb16990ccfafcacb3688d542c659")]
 [assembly: System.Reflection.AssemblyProductAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HD-SUPPORT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
